3fc76bf7f93798589902f109d653b2821a94ec13
Mark Velez <mav7014@nyp.org>
2019-04-25 00:45:53 -0400

    Optimize imports (no functional change)

--- data_steward/validation/main.py
+++ data_steward/validation/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import StringIO
+import datetime
 import json
 import logging
 import os
-import datetime
 
 from flask import Flask
 from googleapiclient.errors import HttpError
@@ -12,13 +12,13 @@ import api_util
 import bq_utils
 import common
 import common_sql
-from common import ACHILLES_EXPORT_PREFIX_STRING, ACHILLES_EXPORT_DATASOURCES_JSON
 import gcs_utils
 import resources
 import validation.achilles as achilles
 import validation.achilles_heel as achilles_heel
 import validation.ehr_union as ehr_union
 import validation.export as export
+from common import ACHILLES_EXPORT_PREFIX_STRING, ACHILLES_EXPORT_DATASOURCES_JSON
 from google.appengine.api.app_identity import app_identity
 
 UNKNOWN_FILE = 'Unknown file'
